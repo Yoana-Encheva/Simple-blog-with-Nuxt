0697ad3379edbbb2cd8a1c3147ccba087d65d1e3
Yoana Encheva <je.encheva@gmail.com>
2020-10-21 17:46:53 +0300

    small changes data and vuex added

--- pages/posts/_id/index.vue
+++ pages/posts/_id/index.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="single-post-page">
     <section class="post">
-      <h1 class="post-title">Title of the Post</h1>
+      <h1 class="post-title">{{ loadedPost.title }}</h1>
       <div class="post-details">
-        <div class="post-detail">Last updated on XXX</div>
-        <div class="post-detail">Written by NAME</div>
+        <div class="post-detail">Last updated on {{ loadedPost.updatedDate }}</div>
+        <div class="post-detail">Written by {{ loadedPost.author }}</div>
       </div>
-      <p class="post-content">Content of the post</p>
+      <p class="post-content">{{ loadedPost.content }}</p>
     </section>
     <section class="post-feedback">
       <p>Let me know what you think about the post, send a mail to <a href="mailto:feedback@my-awesome-domain.com">feedback@my-awesome-domain.com</a>.</p>
     </section>
   </div>
 </template>
+
+<script>
+export default {
+  asyncData(context, callback) {
+    setTimeout(() => {
+      callback(null, {
+        loadedPost: {
+          id: "2",
+          title: "Second Post",
+          author: "Yoana",
+          updatedDate: new Date(),
+          previewText: "This is our second post!",
+          content: "Some dummy content that defenitely is not the text",
+          thumbnail: "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+        }
+      })
+    }, 1000)
+  }
+}
+</script>
 
 <style scoped>
 .single-post-page {
